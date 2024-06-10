dc169d737f5901056637cbd501c1d9d2f80d4c6a
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-10 17:53:03 +0530

    Lab3 print functions arguments and sep

---
+++ py_05062024/Lab003.py
@@ -0,0 +1,24 @@
+# take the input from user and print #print command
+
+# self - conceot in OOps which points to inself
+# *args -  Unlimited number of arguments * - string,int,float,boolean...
+# sep=' ' - How you want to sepereate the arguments
+# end= '\n' - in the end what you want to do
+# file=None - File IO
+
+#args
+print("Hello", "Kishor", 123, True, 3.14)
+print("Hello", "Kishor","Shevkar",sep=" ")
+
+
+#sep # default value is one space
+print("Hello", "Kishor", 123, True, 3.14,sep="_")
+
+#end= '\n'
+print("Hello", "Kishor", 123, True, 3.14,sep="_",end="\t")
+print("Hello", "Kishor", 123, True, 3.14,sep="_")
+
+
+
+
+# To follow proper intentation use the reformation
