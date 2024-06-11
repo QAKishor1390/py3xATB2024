5a9872203df92c7b575d4ead2161f5fdd404f076
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-11 14:47:33 +0530

    Max function with number of arguments

---
+++ py_05062024/Lab007.py
@@ -0,0 +1,32 @@
+#Dynamically typed
+#Dynamic Type- Type of data that python supports.
+
+age=37
+
+# variable name = varisble value
+# identifier = literal
+
+# Type which are supported in the python
+# Integers - Positive and negative whole numbers.
+# Float - Floating point numbers (decimal value)
+print("pi=3.14")
+# String - It is a bunch of characters
+print("Hello Kishor","You are the python automation test engg.","123")
+
+#Boolean = True/False
+print(type(age))
+name= "Kishor"
+isMale=True
+print(type(name))
+
+#Python Complex Number -i iota -
+complex_number= 2+3j\
+    #real - 2
+    #Imaginary = -3
+
+print(complex_number.imag)
+
+a=18.00
+print(a)
+print(type(a))
+
