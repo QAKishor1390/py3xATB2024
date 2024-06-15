dce74433a978c057147070c0049de913f7b71157
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-15 16:26:25 +0530

    String

---
+++ py_07062024/Lan12.py
@@ -0,0 +1,11 @@
+# Raw String
+
+dir = r'C:\kishor\some file'
+print(dir)
+
+# format of the string
+first_name='Kishor'
+last_name='Shevkar'
+print(first_name+" "+last_name)
+print(first_name,last_name)
+print(f'your full name is {first_name} {last_name}')
