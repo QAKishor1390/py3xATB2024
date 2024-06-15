8e061ebd69a634a2da7f1a4e3e06878df2ada3f1
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-15 15:16:09 +0530

    Addition of 2 number

---
+++ py_07062024/Lab11.py
@@ -0,0 +1,19 @@
+# take 2 integer number and sum of them
+
+number1 = int(input("Enter the number1: "))
+number2 = int(input("Enter the number2: "))
+# type conversion
+result = number1 + number2
+print("sum of number is :", result)
+print(type(result))
+
+# Note :
+# + -> int sum operation
+# + -> str - concat operation
+# convert string to int -> int()
+# convert int to string -> str()
+
+a = input("Enter the first no:")
+b = input("Enter the second no:")
+c = int(a) + int(b)
+print("Addition =", c)
