cd56289f484e01dedd46ce4ed4dfa4c4b591f61e
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-17 12:12:44 +0530

    String

---
+++ py_07062024/Lab014.py
@@ -0,0 +1,4 @@
+i = 1
+n = int(input("Enter the value of n: "))
+for i in range(i, n):
+    print(i)
--- py_07062024/Lab13.py
+++ py_07062024/Lab13.py
@@ -1,15 +1,15 @@
 #Format String
 
 print(2*2)
-num = 2
+num = 4
 print(f"The number is {num*1}")
 print(f"The number is {num*2}")
 print(f"The number is {num*3}")
-print(f"The number is {num*4}")
-print(f"The number is {num*5}")
-print(f"The number is {num*6}")
-print(f"The number is {num*7}")
-print(f"The number is {num*8}")
-print(f"The number is {num*9}")
-print(f"The number is {num*10}")
+
+num=5
+print(f"{num}*1={num}")
+print(f"{num}*2={num*2}")
+print(f"{num}*3={num*3}")
+print(f"{num}*4={num*4}")
+print(f"{num}*5={num*5}")
 
