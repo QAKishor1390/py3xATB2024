b0ea54e01dfaf83ce42f9df23cc31eba3260cadd
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-20 13:03:29 +0530

    Function define and call

---
+++ py_17062024/Lab050.py
@@ -0,0 +1,24 @@
+#   Functions :
+#   Block of code - which can executed or reused.
+#   Define
+#   Call
+
+# Builtin Functions - bultins.py - file (Python 3 setup)
+# Which are created by the python contributers
+
+result = max(2, 3)
+print(result)
+
+
+# User Defined
+# They can return something
+# They can't return  -> non return
+# They have parameters
+# They don't parameters / arguments
+
+
+def say_hello():     #Define function
+    print("Hello,Welcome to Python")
+
+
+say_hello()  #Call the function
