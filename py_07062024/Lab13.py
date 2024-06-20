d9b79c653622fc2ef81772595349f6a7a002cd5e
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-20 12:36:13 +0530

    table

--- py_07062024/Lab13.py
+++ py_07062024/Lab13.py
@@ -13,3 +13,8 @@ print(f"{num}*3={num*3}")
 print(f"{num}*4={num*4}")
 print(f"{num}*5={num*5}")
 
+nnum=2
+print(f"The number is {num}")
+print(f"The number is {num*10}")
+print(f"The number is {num*20}")
+print(f"The number is {num*30}")
