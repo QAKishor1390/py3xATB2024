6f9caa9b3f63b65d0244df094710f86cf89dfbc0
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-25 17:36:42 +0530

    SET

--- py_21062024/Lab082.py
+++ py_21062024/Lab082.py
@@ -2,3 +2,10 @@ x = 33
 
 a, b, c = (20, 30, 40)
 print(a, b, c)
+print(a)
+print(b)
+print(c)
+t= (1,2,3)
+
+new_t=t +(4,)
+print(new_t)
---
+++ py_21062024/Lab083.py
@@ -0,0 +1,11 @@
+# SET
+# Collection if items
+# List of unique items (Remove the duplicate)
+
+
+list = {1, 2, 3, 4, 4, 5}
+print(list)
+
+set1 = set(list)
+print(set1)
+print(len(set1))
