7429a68bc5bd41023e691c7cf5178e8e9c966b40
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-25 16:37:13 +0530

    Tuples : - Tuple , Tuple conversion - Tuple within tuple

---
+++ py_21062024/Lab080.py
@@ -0,0 +1,19 @@
+# Tuple
+# Decorators
+
+# We use Tuples in APIs
+
+APT_URLSs = ("https://www.google.com", "https://facebook.com")
+print(APT_URLSs[0])
+print(APT_URLSs[1])
+print(APT_URLSs)
+
+t = tuple()
+print(t)
+
+# Conversion List to Tuple
+list=["Kishor", "Pooja", "Nitya"]
+t1 = tuple(list)
+print(t1)
+
+
---
+++ py_21062024/Lab081.py
@@ -0,0 +1,6 @@
+tuple1 = ("Kishor", "Nitya", "Pooja")
+tuple2 = ("Kishor", "Nitya", "Varad", "Pooja")
+tuple3 = (tuple1, tuple2)
+print(tuple3)
+print(tuple1[1])
+print(tuple2[2])
