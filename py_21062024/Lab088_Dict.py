322b2700fd2b31e14271dae7b7cbf355b9716775
Kishor1390 <kishorshevkar1390@gmail.com>
2024-06-27 15:34:49 +0530

    Dict

---
+++ py_21062024/Lab088_Dict.py
@@ -0,0 +1,13 @@
+# Dict : Dic
+# Key and value
+# name -> "Kishor"
+
+
+my_dict = {"name": "Kishor", "Age": 32, "Address": "Pune"}
+# print(len(my_dict))
+print(my_dict["name"])
+print(my_dict["Age"])
+print(my_dict["Address"])
+
+phone_book = dict(name="Nitya", age=4, address="Pune")
+print(phone_book)
---
+++ py_21062024/Lab089_Dict.py
@@ -0,0 +1,21 @@
+# Dict : Dic
+# Key and value
+# name -> "Kishor"
+
+
+my_dict = {"name": "Kishor", "Age": 32, "Address": "Pune"}
+# # print(len(my_dict))
+# print(my_dict["name"])
+# print(my_dict["Age"])
+# print(my_dict["Address"])
+
+# phone_book = dict(name="Nitya", age=4, address="Pune")
+# print(phone_book)
+
+print(my_dict["name"])
+my_dict["name"] = "Sakshi"
+my_dict["Age"] = 24
+my_dict["Address"]="Goa"
+print(my_dict["name"])
+print(my_dict["Age"])
+print(my_dict["Address"])
