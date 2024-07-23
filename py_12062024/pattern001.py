b8eece621b7362f1f33b7ea9fd28f35c888fef69
Kishor1390 <kishorshevkar1390@gmail.com>
2024-07-23 17:52:57 +0530

    Pytest Framework

--- py_12062024/pattern001.py
+++ py_12062024/pattern001.py
@@ -7,7 +7,7 @@
 
 rows=int(input("Enter the number of rows: "))
 for i in range(1,rows+1):
-    for j in range(i,rows + i):
+    for j in range(i):
         print(i,end=" ")
     print()
 
---
+++ py_22072024/test_Lab170.py
@@ -0,0 +1,19 @@
+def test_addition():
+    assert 1 + 1 == 2
+
+
+def test_subtraction():
+    assert 8 - 4 == 4
+
+def test_mul():
+        assert 8 * 4 == 32
+
+    ### //pytest framework
+    # - pip install pytest
+    # - File name - test_name.py (test_Lab170)
+    # - Test name - test_name_of_test():   (test_addition():)
+    # - Assert - Actual result == expected result
+
+    # ### How to run the Pytest ?
+    # - open
+    # #cmd -> go the  folder - pytest
